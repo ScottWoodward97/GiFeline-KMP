c333a42576bc00897d397b594dbaa8296417858b
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-07 18:43:38 +0100

    import cleanup

--- app/src/main/java/uk/co/sw/gifeline/GiFelineActivity.kt
+++ app/src/main/java/uk/co/sw/gifeline/GiFelineActivity.kt
@@ -3,16 +3,7 @@ package uk.co.sw.gifeline
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
-import uk.co.sw.gifeline.feature.navigation.GiFelineNavHost
 import uk.co.sw.gifeline.feature.navigation.GiFelineScreen
 import uk.co.sw.gifeline.ui.theme.GiFelineTheme
 
