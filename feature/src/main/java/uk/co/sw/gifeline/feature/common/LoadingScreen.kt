a32c7401a6d3161c452fd65bb406674e978e5828
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-12 12:14:36 +0100

    add custom (cat) loading spinner

--- feature/src/main/java/uk/co/sw/gifeline/feature/common/LoadingScreen.kt
+++ feature/src/main/java/uk/co/sw/gifeline/feature/common/LoadingScreen.kt
@@ -3,12 +3,13 @@ package uk.co.sw.gifeline.feature.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uk.co.sw.gifeline.feature.home.SpinningCat
 import uk.co.sw.gifeline.feature.theme.GiFelineTheme
 
 @Composable
@@ -19,7 +20,7 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        SpinningCat(size = 48.dp)
     }
 }
 
