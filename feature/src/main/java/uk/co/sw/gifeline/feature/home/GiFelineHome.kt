9d47b2dc3295d4bc355d7351457105333508d911
Scott Woodward <32957808+ScottWoodward97@users.noreply.github.com>
2024-05-12 18:40:21 +0100

    Add custom theming (#6)
    * add custom (cat) loading spinner
    
    * add lore to homepage
    
    * make loading spinner dark mode compatible
    
    * add custom google font
    
    * adjust button tet styling on home
    
    * add custom material theme (using material theme builder)
    
    * update existing screens to utilise new theme

--- feature/src/main/java/uk/co/sw/gifeline/feature/home/GiFelineHome.kt
+++ feature/src/main/java/uk/co/sw/gifeline/feature/home/GiFelineHome.kt
@@ -1,9 +1,12 @@
 package uk.co.sw.gifeline.feature.home
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,29 +14,94 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uk.co.sw.gifeline.feature.R
+import uk.co.sw.gifeline.feature.common.surfaceContainer
+import uk.co.sw.gifeline.feature.common.tertiaryContainer
 import uk.co.sw.gifeline.feature.theme.GiFelineTheme
 
 @Composable
 fun GiFelineHome(
-    onNavigateToBreedSelector:() -> Unit,
+    onNavigateToBreedSelector: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Button(onClick = onNavigateToBreedSelector) {
-            Text(text = stringResource(R.string.home_select_breed_action))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.home_title),
+                style = MaterialTheme.typography.displayLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Text(
+                text = stringResource(R.string.home_sub_title),
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .surfaceContainer()
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .surfaceContainer()
+                .padding(8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.home_select_breed_body),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                modifier = Modifier.tertiaryContainer()
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onNavigateToBreedSelector,
+            ) {
+                Text(
+                    text = stringResource(R.string.home_select_breed_action),
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+        Column(
+            modifier = Modifier.surfaceContainer(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = stringResource(R.string.home_small_print),
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+            Text(
+                text = stringResource(R.string.home_copyright),
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
         }
     }
 }
 
 @Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun GiFelineHomePreview() {
     GiFelineTheme {
---
+++ feature/src/main/java/uk/co/sw/gifeline/feature/home/SpinningCat.kt
@@ -0,0 +1,75 @@
+package uk.co.sw.gifeline.feature.home
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import uk.co.sw.gifeline.feature.R
+import uk.co.sw.gifeline.feature.theme.GiFelineTheme
+
+@Composable
+fun SpinningCat(size: Dp) {
+    val transition = rememberInfiniteTransition()
+    val rotation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            tween(
+                durationMillis = 2000,
+                easing = LinearEasing,
+            )
+        ),
+        label = ""
+    )
+    val animatedScale by transition.animateFloat(
+        initialValue = 0.75f,
+        targetValue = 1.25f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = LinearEasing,
+            ),
+            repeatMode = RepeatMode.Reverse,
+        ),
+        label = ""
+    )
+    Box(Modifier.size(size * 1.25f), contentAlignment = Alignment.Center) {
+        Icon(
+            painter = painterResource(id = R.drawable.cat),
+            contentDescription = null,
+            modifier = Modifier
+                .rotate(rotation)
+                .graphicsLayer {
+                    scaleX = animatedScale
+                    scaleY = animatedScale
+                    transformOrigin = TransformOrigin.Center
+                },
+            tint = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SpinningCatPreview() {
+    GiFelineTheme {
+        SpinningCat(100.dp)
+    }
+}
