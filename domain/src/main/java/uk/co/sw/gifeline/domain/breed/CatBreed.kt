1c3482003f7e8eca7416820c0557a9bc936b1699
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-14 00:19:59 +0100

    ingest new breed data from endpoint

--- domain/src/main/java/uk/co/sw/gifeline/domain/breed/CatBreed.kt
+++ domain/src/main/java/uk/co/sw/gifeline/domain/breed/CatBreed.kt
@@ -4,4 +4,16 @@ data class CatBreed(
     val id: String,
     val name: String,
     val altNames: List<String>,
-)
+    val origin: String,
+    val description: String,
+    val lifeSpan: String,
+    val weight: String,
+    val stats: Stats,
+    val wikiUrl: String?,
+) {
+    data class Stats(
+        val energyLevel: Int,
+        val intelligence: Int,
+        val vocalisation: Int,
+    )
+}
