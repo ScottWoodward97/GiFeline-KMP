1724aff0753cd4065025bf7ea1ddea94d436823d
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-13 11:55:36 +0100

    migrate image repository to use abstract repository

--- data/src/main/java/uk/co/sw/gifeline/data/images/CatImageRepository.kt
+++ data/src/main/java/uk/co/sw/gifeline/data/images/CatImageRepository.kt
@@ -1,29 +1,19 @@
 package uk.co.sw.gifeline.data.images
 
-import retrofit2.HttpException
+import uk.co.sw.gifeline.data.common.Repository
 import uk.co.sw.gifeline.data.common.response.ApiResponse
 import javax.inject.Inject
 
 class CatImageRepository @Inject constructor(
     private val catImagesService: CatImagesService,
-) {
+) : Repository() {
 
     suspend fun getCatImages(
         breedId: String,
         page: Int = PAGE,
         limit: Int = LIMIT,
     ): ApiResponse<List<CatImagesEntity>> {
-        return try {
-            val response = catImagesService.getImages(breedId, page, limit)
-            val body = response.body()
-            if (response.isSuccessful && body != null) {
-                ApiResponse.Success(body)
-            } else {
-                ApiResponse.Error(HttpException(response))
-            }
-        } catch (e: Exception) {
-            ApiResponse.Error(e)
-        }
+        return handleResponse { catImagesService.getImages(breedId, page, limit) }
     }
 
     private companion object {
