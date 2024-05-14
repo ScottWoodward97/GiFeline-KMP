94aa79d3126a70d6cbf2e67277fffadc55724f0f
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-14 15:35:42 +0100

    open wiki url in bottom sheet

---
+++ feature/src/main/java/uk/co/sw/gifeline/feature/breedprofile/profile/WebBottomSheet.kt
@@ -0,0 +1,43 @@
+package uk.co.sw.gifeline.feature.breedprofile.profile
+
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WebBottomSheet(
+    url: String,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+        modifier = modifier.fillMaxSize()
+    ) {
+        LazyColumn {
+            item {
+                AndroidView(factory = {
+                    WebView(it).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        webViewClient = WebViewClient()
+                    }
+                }, update = {
+                    it.loadUrl(url)
+                })
+            }
+        }
+    }
+}
