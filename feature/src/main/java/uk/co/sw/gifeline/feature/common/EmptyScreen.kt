b013f62f7207f0b6339224f21622192aa12b4296
ScottWoodward97 <scottwoodward97@gmail.com>
2024-05-14 21:50:15 +0100

    migrate empty state copy to resource

--- feature/src/main/java/uk/co/sw/gifeline/feature/common/EmptyScreen.kt
+++ feature/src/main/java/uk/co/sw/gifeline/feature/common/EmptyScreen.kt
@@ -13,8 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import uk.co.sw.gifeline.feature.R
 import uk.co.sw.gifeline.feature.theme.GiFelineTheme
 
 @Composable
@@ -35,8 +38,9 @@ fun EmptyScreen(
             modifier = Modifier.size(64.dp)
         )
         Text(
-            text = "Nothing here",
+            text = stringResource(R.string.empty_screen_message),
             style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
         )
     }
 }
